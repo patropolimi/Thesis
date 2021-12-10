6c3ac7685d0de484ef70a39d291a5cd5794a0c28
Riccardo Patroni <patropolimi@icloud.com>
2021-12-10 13:33:21 +0100

    Upload Last Tests (LeakyRelu, Silu) + (Regularized Poisson Saw-Teeth) & Reordering

--- Basic/Sensitivity/Generic/Test_5/Launch_Script.py
+++ Basic/Sensitivity/Generic/Test_5/Launch_Script.py
@@ -6,54 +6,48 @@ from Basic.PINN_Resolutors import *
 """ Test 5 Launch Script -> Basic PINN Generic Sensitivity Analysis"""
 
 
+Negative_Slope=5*1e-2
+def LeakyRelu(X):
+	return jax.nn.leaky_relu(X,Negative_Slope)
 def F(X):
-	return (jnp.sum(jnp.zeros_like(X),axis=0))[None,:]
+	return (40000*(X**3-(2/3)*(X**2)+(173/1800)*X+(1/300))*jnp.exp(-100*((X-1/3)**2)))
 def S(X):
-	return (1.0-jnp.tanh((1.0/5e-3)*(X[1,:]-X[0,:])))[None,:]
-String='1-tanh(200*(x-t))'
+	return (X*(jnp.exp(-((X-1/3)/(1/10))**2)-jnp.exp(-((2/3)/(1/10))**2)))
+String='x*(exp(-((x-1/3)/(1/10))**2)-exp(-((2/3)/(1/10))**2))'
 def G(X):
-	return (1.0-jnp.tanh((1.0/5e-3)*(X[1,:]-X[0,:])))[None,:]
+	return jnp.zeros_like(X)
 NAttempts=3
-Number_Residuals=[400,800]
-ADAM_Steps=25000
-LBFGS_MaxSteps=225000
-Limits=np.array([[0.0,1.0],[0.0,1.0]])
-Points_T=np.linspace(0.0,1.0,1001)
-Points_X=np.linspace(0.0,1.0,1001)
-NT=Points_T.shape[0]
-NX=Points_X.shape[0]
-TV=Points_T[None,:]
-Points=[]
-for i in range(NX):
-	XV=np.array(NT*[Points_X[i]])[None,:]
-	Points+=[np.concatenate((TV,XV),axis=0)]
-Points=np.concatenate(Points,axis=1)
-Number_Boundary_Points=[[200,0],[100,100]]
-Boundary_Labels=['Dirichlet','None','Dirichlet','Dirichlet']
+Number_Residuals=[10,20,40,80]
+ADAM_Steps=15000
+LBFGS_MaxSteps=35000
+Limits=np.array([[0.0,1.0]])
+Points=np.linspace(0.0,1.0,1001)
+Number_Boundary_Points=[[1,1]]
+Boundary_Labels=2*['Dirichlet']
 Hidden_Layers=[1,2]
-Neurons_Per_Layer=[25,50,100]
+Neurons_Per_Layer=[10,20,40,80]
 Initialization='Glorot_Uniform'
-Activations={'Tanh': jnp.tanh,'Relu': jax.nn.relu}
-Problem=Burgers_Scalar_Basic
+Activations={'Tanh': jnp.tanh,'Relu': jax.nn.relu,'Silu': jax.nn.silu,'LeakyRelu': LeakyRelu}
+Problem=Poisson_Scalar_Basic
 Parameters=None
 for NR in Number_Residuals:
 	for SigmaName,Sigma in Activations.items():
 		for HL in Hidden_Layers:
 			for NPL in Neurons_Per_Layer:
 				for i in range(NAttempts):
-					Architecture={'Input_Dimension': 2,'Hidden_Layers': HL,'Neurons_Per_Layer': HL*[NPL],'Activation': Sigma,'Initialization': Initialization}
-					Domain={'Dimension': 2,'Limits': Limits,'Number_Residuals': NR,'Number_Boundary_Points': Number_Boundary_Points,'Boundary_Labels': Boundary_Labels}
-					Data={'Source': F,'Exact_Dirichlet': G,'Exact_Neumann': None,'Nu': 2.5e-3}
+					Architecture={'Input_Dimension': 1,'Hidden_Layers': HL,'Neurons_Per_Layer': HL*[NPL],'Activation': Sigma,'Initialization': Initialization}
+					Domain={'Dimension': 1,'Limits': Limits,'Number_Residuals': NR,'Number_Boundary_Points': Number_Boundary_Points,'Boundary_Labels': Boundary_Labels}
+					Data={'Source': F,'Exact_Dirichlet': G,'Exact_Neumann': None}
 					Solver=Resolutor_Basic[Problem](Architecture,Domain,Data)
 					Start=time.perf_counter()
 					History=Solver.Learn(ADAM_Steps,LBFGS_MaxSteps,Parameters)
 					End=time.perf_counter()
 					Elapsed=End-Start
-					Network_Eval=Solver.Network_Multiple(Points,Solver.Architecture['W'])
+					Network_Eval=Solver.Network_Multiple(Points[None,:],Solver.Architecture['W'])[0,:]
 					Solution_Eval=S(Points)
 					Error=Solution_Eval-Network_Eval
 					Relative_L2_Error=np.linalg.norm(Error)/np.linalg.norm(Solution_Eval)
-					Dictionary={'Time': Elapsed,'History': History,'Relative_L2_Error': Relative_L2_Error,'Solution': String,'Network_Eval': Network_Eval,'Solution_Eval': Solution_Eval,'Points_Abscissa': Points_T,'Points_Ordinate': Points_X}
+					Dictionary={'Time': Elapsed,'History': History,'Relative_L2_Error': Relative_L2_Error,'Solution': String,'Network_Eval': Network_Eval,'Solution_Eval': Solution_Eval,'Points_Eval': Points}
 					Name=SigmaName+'_Model_'+str(HL)+'HL_'+str(NPL)+'NPL_'+str(NR)+'NR_'+str(i+1)
 					File=open(Name,'wb')
 					dill.dump(Dictionary,File)
